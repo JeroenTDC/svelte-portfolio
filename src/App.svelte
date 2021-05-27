c65d092e4799c0d9bda6d52bf96101f8a198943f
Jeroen Marco Frank Stolk <JMST@nuuday.dk>
2021-05-27 14:43:40 +0200

    up

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,9 @@
 <script>
+	import Navigation from './Components/Navigation2.svelte';
 	export let name;
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<Navigation/>
 
 <style>
 	main {
@@ -15,7 +13,7 @@
 		margin: 0 auto;
 	}
 
-	h1 {
+	:global(h1) {
 		color: #ff3e00;
 		text-transform: uppercase;
 		font-size: 4em;
---
+++ src/Components/Navigation.svelte
@@ -0,0 +1,32 @@
+<script>
+  //import data from "./items.json";
+  import About from "../Pages/About/About.svelte";
+  import Address from "../Pages/Address/Address.svelte";
+
+  let data = {
+    pages: [
+      {
+        title: "About us",
+        path: "About",
+      },
+      {
+        title: "Our address",
+        path: "Address",
+      },
+    ],
+  };
+
+  let renderedPage = Address;
+
+  function selectPage() {
+    renderedPage === Address ? About : Address;
+  }
+</script>
+
+<div>
+  {#each data.pages as page}
+  <button on:click="{selectPage}">{page.path}</button>
+  {/each}
+</div>
+
+<svelte:component this="{renderedPage}" />
